fe9de33782043a5a7df9f582b7d34972cfaca725
icalll <zalsujana07@gmail.com>
2024-05-07 08:06:05 +0700

    up

--- Front End/src/components/common/DynamicShow.jsx
+++ Front End/src/components/common/DynamicShow.jsx
@@ -1,52 +1,15 @@
-import React, { useState } from "react";
-import PhoneInput from "react-phone-number-input";
-import Select from 'react-select';
-import makeAnimated from 'react-select/animated';
-import { ReactComponent as EyeSlashIcon } from "../../assets/icon/ic_eye-slash.svg";
-import { ReactComponent as EyeIcon } from "../../assets/icon/ic_eye.svg";
-import useTheme from "../context/useTheme";
+import React from "react";
 
-import { Editor } from 'react-draft-wysiwyg';
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
-import Datepicker from "react-tailwindcss-datepicker";
 import ImageComponent from "../../utils/helpers/getImageURL";
 
 function DynamicShow({
   label,
-  icon,
-  name,
   value,
   options,
-  color,
   type,
-  disabled,
-  placeholder,
-  onChange,
   className,
 }) {
-  const [isPassword, setIsPassword] = useState(false);
-  const { isDarkMode } = useTheme();
-  const [image, setImage] = useState(null);
-
-  const animatedComponents = makeAnimated();
-  const handlePhoneInputChange = (inputValue) => {
-    if (inputValue?.startsWith("+0")) {
-      inputValue = "+62" + inputValue.slice(2);
-    }
-
-    onChange(inputValue);
-  };
-
-  const handleImageChange = (event) => {
-    const selectedImage = event.target.files[0];
-    setImage(URL.createObjectURL(selectedImage));
-    onChange(selectedImage);
-  };
-  const handleChangeInputArray = (index, event) => {
-    const updatedValue = [...value];
-    updatedValue[index] = { ...updatedValue[index], value: event.target.value };
-    onChange(updatedValue);
-  };
 
   let parsedOptions = [];
   try {
