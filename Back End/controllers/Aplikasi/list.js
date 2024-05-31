3ddfb3826a24d9abe5c51259bde4439cb0948b5f
icalll <zalsujana07@gmail.com>
2024-05-31 14:48:04 +0700

    up

---
+++ Back End/controllers/Aplikasi/list.js
@@ -0,0 +1,105 @@
+import InfraModel from "../../models/InfraModel.js";
+
+export const getListDataAplikasi = async(req, res) => {
+    try {
+        const { role } = req.body;
+        const apiKey = req.headers["x-api-key"];
+        if (!apiKey) {
+            return res.status(401).json({
+                status: "error",
+                msg: "API Key is required",
+            });
+        } else {
+            const infrastruktur = await InfraModel.findAll();
+            infrastruktur.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+
+            const filteredinfrastruktur = infrastruktur.filter((item) => {
+                if (!item.role) return false;
+                const itemRoles = JSON.parse(item.role);
+                return itemRoles.includes(role);
+            });
+
+            if (role === "perangkat_daerah") {
+                const validinfrastruktur = filteredinfrastruktur.filter(
+                    (item) => item.apiKey === apiKey
+                );
+
+                if (validinfrastruktur.length === 0) {
+                    return res.status(400).json({
+                        status: "error",
+                        msg: "Item not found",
+                    });
+                }
+
+                const totalItemsByStatus = {
+                    divalidasi: filteredinfrastruktur.filter(
+                        (user) => user.submission_status === 2 || user.submission_status === 4
+                    ).length,
+                    diproses: filteredinfrastruktur.filter(
+                        (user) => user.submission_status === 6
+                    ).length,
+                    ditolak: filteredinfrastruktur.filter(
+                        (user) => user.submission_status === 3 || user.submission_status === 5 || user.submission_status === 8
+                    ).length,
+                    disetujui: filteredinfrastruktur.filter(
+                        (user) => user.submission_status === 7
+                    ).length,
+                };
+                res.json({
+                    status: "ok",
+                    msg: "Data infrastruktur retrieved successfully",
+                    data: validinfrastruktur.map((item) => {
+                        const fields = JSON.parse(item.fields);
+                        return {
+                            id: item.id,
+                            name_pic: fields.name_pic,
+                            submission_title: item.submission_title,
+                            submission_status: item.submission_status,
+                            createdAt: item.createdAt,
+                        };
+                    }),
+                    totalItems: validinfrastruktur.length,
+                    totalItemsByStatus: totalItemsByStatus,
+                });
+            } else {
+                // Jika bukan peran perangkat_daerah, kembalikan data tanpa validasi API key
+                const totalItemsByStatus = {
+                    divalidasi: filteredinfrastruktur.filter(
+                        (user) => user.submission_status === 2 || user.submission_status === 4
+                    ).length,
+                    diproses: filteredinfrastruktur.filter(
+                        (user) => user.submission_status === 6
+                    ).length,
+                    ditolak: filteredinfrastruktur.filter(
+                        (user) => user.submission_status === 3 || user.submission_status === 5 || user.submission_status === 8
+                    ).length,
+                    disetujui: filteredinfrastruktur.filter(
+                        (user) => user.submission_status === 7
+                    ).length,
+                };
+                res.json({
+                    status: "ok",
+                    msg: "Data infrastruktur retrieved successfully",
+                    data: filteredinfrastruktur.map((item) => {
+                        const fields = JSON.parse(item.fields);
+                        return {
+                            id: item.id,
+                            name_pic: fields.name_pic,
+                            submission_title: item.submission_title,
+                            submission_status: item.submission_status,
+                            createdAt: item.createdAt,
+                        };
+                    }),
+                    totalItems: filteredinfrastruktur.length,
+                    totalItemsByStatus: totalItemsByStatus,
+                });
+            }
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            status: "error",
+            msg: "Internal Server Error",
+        });
+    }
+};
--- Back End/routes/index.js
+++ Back End/routes/index.js
@@ -1,13 +1,13 @@
 import express from "express";
 import {
-  checkRoleUser,
-  createUsers,
-  deleteUsers,
-  getListUser,
-  getUser,
-  getUserById,
-  //   Register,
-  Login,
+    checkRoleUser,
+    createUsers,
+    deleteUsers,
+    getListUser,
+    getUser,
+    getUserById,
+    //   Register,
+    Login,
 } from "../controllers/Users.js";
 import { verifyToken } from "../middleware/VerifyToken.js";
 import { uploadImages } from "../components/UploadImage.js";
@@ -15,22 +15,43 @@ import validateImage from "../middleware/Multer.js";
 import validatePDF from "../middleware/Multer2.js";
 import { uploadFiles } from "../components/UploadFile.js";
 
-import { getListInfrastruktur } from "../controllers/Infrastruktur/listInfrastruktur.js";
-import { getDetailInfrastruktur } from "../controllers/Infrastruktur/detailInfrastruktur.js";
-import { editProcessInfrastruktur, setInfrastruktur } from "../controllers/Infrastruktur/index.js";
-import { editInfrastruktur } from "../controllers/Infrastruktur/editInfrastruktur.js";
-import { deleteInfrastruktur } from "../controllers/Infrastruktur/deleteInfrastruktur.js";
+import { getListDataAplikasi } from "../controllers/Aplikasi/list.js";
+import { getDetailDataAplikasi } from "../controllers/Aplikasi/detail.js";
+import {
+    editProcessDataAplikasi,
+    setStatusDataAplikasi,
+} from "../controllers/Aplikasi/index.js";
+import { editDataAplikasi } from "../controllers/Aplikasi/edit.js";
+import { deleteDataAplikasi } from "../controllers/Aplikasi/delete.js";
+
+import { getListDataInfrastruktur } from "../controllers/Infrastruktur/list.js";
+import { getDetailDataInfrastruktur } from "../controllers/Infrastruktur/detail.js";
+import {
+    editProcessDataInfrastruktur,
+    setStatusDataInfrastruktur,
+} from "../controllers/Infrastruktur/index.js";
+import { editDataInfrastruktur } from "../controllers/Infrastruktur/edit.js";
+import { deleteDataInfrastruktur } from "../controllers/Infrastruktur/delete.js";
 
 const router = express.Router();
 
-router.post("/infrastruktur", verifyToken, getListInfrastruktur);
-router.post("/infrastruktur/detail", verifyToken, getDetailInfrastruktur);
-router.post("/infrastruktur/create", verifyToken, setInfrastruktur);
-router.post("/infrastruktur/set_process", verifyToken, editProcessInfrastruktur);
-router.post("/infrastruktur/edit", verifyToken, editInfrastruktur);
-router.post("/infrastruktur/delete", verifyToken, deleteInfrastruktur);
+router.post("/infrastruktur", verifyToken, getListDataInfrastruktur);
+router.post("/infrastruktur/detail", verifyToken, getDetailDataInfrastruktur);
+router.post("/infrastruktur/create", verifyToken, setStatusDataInfrastruktur);
+router.post(
+    "/infrastruktur/set_process",
+    verifyToken,
+    editProcessDataInfrastruktur
+);
+router.post("/infrastruktur/edit", verifyToken, editDataInfrastruktur);
+router.post("/infrastruktur/delete", verifyToken, deleteDataInfrastruktur);
 
-
+router.post("/aplikasi", verifyToken, getListDataAplikasi);
+router.post("/aplikasi/detail", verifyToken, getDetailDataAplikasi);
+router.post("/aplikasi/create", verifyToken, setStatusDataAplikasi);
+router.post("/aplikasi/set_process", verifyToken, editProcessDataAplikasi);
+router.post("/aplikasi/edit", verifyToken, editDataAplikasi);
+router.post("/aplikasi/delete", verifyToken, deleteDataAplikasi);
 
 router.post("/me", verifyToken, getUser);
 router.post("/list_users", verifyToken, getListUser);
@@ -47,17 +68,17 @@ router.post("/login", Login);
 // router.delete("/logout", Logout);
 
 router.post(
-  "/upload_images",
-  verifyToken,
-  validateImage.single("file"),
-  uploadImages
+    "/upload_images",
+    verifyToken,
+    validateImage.single("file"),
+    uploadImages
 );
 
 router.post(
-  "/upload_files",
-  verifyToken,
-  validatePDF.single("file"),
-  uploadFiles
+    "/upload_files",
+    verifyToken,
+    validatePDF.single("file"),
+    uploadFiles
 );
 
 export default router;
