3de35b9bdc73c819116291c35e1399acf22373a0
Muhamad Iqbal Ramadan <141218560+MIqbalRamadan@users.noreply.github.com>
2024-05-31 15:46:31 +0700

    UP

--- Front End/src/pages/Layanan Pengelolaan Sistem Informasi dan Keamanan Jaringan/validators.jsx
+++ Front End/src/pages/Layanan Pengelolaan Sistem Informasi dan Keamanan Jaringan/validators.jsx
@@ -1,76 +1,13 @@
 import { validateAddress, validateArray, validateFullname, validateHTML, validateImage, validateIPAddress, validatePeriod, validatePeriod1, validateRadioBottom, validateTelp, validateText, validateTextArea } from "../../utils/helpers/validateForm";
 
-export const isValidatorRelokasiAlat = (obj) => {
+export const isValidatorIntegrasi = (obj) => {
   let isValid = true;
   isValid = isValid && validateFullname(obj.name_pic, "Nama PIC");
   isValid = isValid && validateTelp(obj.telp_pic, "Nomor PIC");
-  isValid = isValid && validateArray(obj.type_tools, "Jenis Alat yang direlokasikan");
-  isValid = isValid && validateText(obj.distance_estimation, "Estimati Jarak");
-  isValid = isValid && validateHTML(obj.reason, "Alasan Pengajuan");
-  isValid = isValid && validateRadioBottom(obj.status, 'Status');
+  isValid = isValid && validateArray(obj.app_name, "Nama Aplikasi");
+  isValid = isValid && validateHTML(obj.app_desc, "Deskripsi Aplikasi");
+  isValid = isValid && validateHTML(obj.needed_data, "Data Yang di Butuhkan");
+  isValid = isValid && validateHTML(obj.integration, "Tujuan Integrasi");
 
   return isValid;
 };
-
-export const isValidatorPenambahanAlat = (obj) => {
-  let isValid = true;
-  isValid = isValid && validateFullname(obj.name_pic, "Nama PIC");
-  isValid = isValid && validateTelp(obj.telp_pic, "Nomor PIC");
-  isValid = isValid && validateArray(obj.type_tools, "Jenis Alat yang dibutuhkan");
-  isValid = isValid && validateHTML(obj.reason, "Alasan Pengajuan");
-  isValid = isValid && validateText(obj.distance_estimation, "Estimati Jarak");
-
-  return isValid;
-};
-
-export const isValidatorPenambahanBandwith = (obj) => {
-  let isValid = true;
-  isValid = isValid && validateFullname(obj.name_pic, "Nama PIC");
-  isValid = isValid && validateTelp(obj.telp_pic, "Nomor PIC");
-  isValid = isValid && validateArray(obj.initial_bandwith, "Bandtwith Awal");
-  isValid = isValid && validateArray(obj.proposed_bandwidth, "Bandtwith Usulan");
-  isValid = isValid && validateHTML(obj.reason, "Alasan Pengajuan");
-  isValid = isValid && validatePeriod1(obj.period, "Periode Jangka Waktu");
-
-  // if (obj.status_BDO === 'temporary') {
-  //   isValid = isValid && validatePeriod(obj.period, "Periode Jangka Waktu");
-  // }
-
-  return isValid;
-};
-
-export const isValidatorTroubleShooting = (obj) => {
-  let isValid = true;
-  isValid = isValid && validateFullname(obj.name_pic, "Nama PIC");
-  isValid = isValid && validateTelp(obj.telp_pic, "Nomor PIC");
-  isValid = isValid && validateText(obj.incident, "Waktu Kejadian");
-  isValid = isValid && validateHTML(obj.reason, "Alasan Pengajuan");
-  isValid = isValid && validateImage(obj.image_screenshoot, "Screenshot");
-
-  return isValid;
-};
-
-export const isValidatorHosting = (obj) => {
-  let isValid = true;
-  isValid = isValid && validateFullname(obj.name_pic, "Nama PIC");
-  isValid = isValid && validateTelp(obj.telp_pic, "Nomor PIC");
-  isValid = isValid && validateArray(obj.app, "Nama Aplikasi");
-  isValid = isValid && validateTextArea(obj.other_requirements, "Requirement Lainnya");
-  isValid = isValid && validateHTML(obj.reason, "Alasan Pengajuan");
-
-  return isValid;
-};
-
-export const isValidatorDomain = (obj) => {
-  let isValid = true;
-  isValid = isValid && validateFullname(obj.name_pic, "Nama PIC");
-  isValid = isValid && validateTelp(obj.telp_pic, "Nomor PIC");
-  isValid = isValid && validateArray(obj.app, "Nama Aplikasi");
-  isValid = isValid && validateText(obj.domain_name, "Usulan Domain");
-  isValid = isValid && validateIPAddress(obj.ip_address, "IP Address");
-  isValid = isValid && validateHTML(obj.reason, "Alasan Pengajuan");
-
-  return isValid;
-};
-
-
