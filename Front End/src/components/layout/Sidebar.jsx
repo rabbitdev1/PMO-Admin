649b06dde965dda51f8b461945b6d25f8854d316
icalll <zalsujana07@gmail.com>
2024-06-03 09:51:15 +0700

    up

--- Front End/src/components/layout/Sidebar.jsx
+++ Front End/src/components/layout/Sidebar.jsx
@@ -61,6 +61,7 @@ const Sidebar = () => {
     {
       title: "Layanan Pengelolaan Sistem Informasi dan Keamanan Jaringan", role: ['op_pmo', 'perangkat_daerah','kabid_aplikasi','teknis_aplikasi','katim_aplikasi'],
       submenu: [
+        { title: 'Permohonan SI', href: "/layanan-pengelolaan-sistem-informasi-dan-keamanan-jaringan", state: 'Permohonan SI' },
         { title: 'User Akun Sistem Informasi', href: "/layanan-pengelolaan-sistem-informasi-dan-keamanan-jaringan", state: 'User Akun Sistem Informasi' },
         { title: 'Intergrasi Sistem Informasi', href: "/layanan-pengelolaan-sistem-informasi-dan-keamanan-jaringan", state: 'Integrasi Sistem Informasi' },
         { title: 'Penerapan Modul TTE', href: "/layanan-pengelolaan-sistem-informasi-dan-keamanan-jaringan", state: 'Penerapan Modul TTE' },
